ff94de649b2ec7b7943c57b5d4d92cfd2fa2747d
janzu93 <janne.lofhjelm@gmail.com>
2017-09-30 06:08:02 +0300

    Korjattu testidata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,7 +5,7 @@ INSERT INTO Kayttaja(etunimi, sukunimi, kayttajatunnus, salasana, syntymapaiva, 
  VALUES ('Matti', 'Maksaja', 'MaMa', 'peelo123', '1998-10-10', 'Testitie 5D, 02110 Helsinki');
 
 INSERT INTO Kayttaja(etunimi, sukunimi, kayttajatunnus, salasana, syntymapaiva, osoite)
- VALUES ('Teemu', 'Testeri', 'Tester123', 'test2552552550', '1993-12-12', 'Lamppulankuja 17C, 01000 Helsinki')
+ VALUES ('Teemu', 'Testeri', 'Tester123', 'test2552552550', '1993-12-12', 'Lamppulankuja 17C, 01000 Helsinki');
 
 INSERT INTO Ilmoitus(nimi, alkamispaiva, paattymispaiva, lahtohinta, hintaNyt, kuvaus, kayttaja_id)
  VALUES('Ladidas kenkä (pariton)', '2017-09-09', '2017-09-20', 20.00, 50.00,'Myydään tarpeettomana Ladidaksen pariton kenkä. Vain tosissaan huutavia kiitos!',
